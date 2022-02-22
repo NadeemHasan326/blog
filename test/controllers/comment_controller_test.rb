efbb21a7b27edb4a5d6c1433f926320f4eada0e3
Nadeem Hasan <nadeemhasan326@gmail.com>
2022-02-22 16:55:43 +0530

    this is blog app

---
+++ test/controllers/comment_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class CommentControllerTest < ActionDispatch::IntegrationTest
+  test "should get commenter:string" do
+    get comment_commenter:string_url
+    assert_response :success
+  end
+
+  test "should get body:text" do
+    get comment_body:text_url
+    assert_response :success
+  end
+
+  test "should get article:reference" do
+    get comment_article:reference_url
+    assert_response :success
+  end
+end
